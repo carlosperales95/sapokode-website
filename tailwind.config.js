57c12e3814a21d38f069576c98bb5d49f112fcd8
carlospe <carlos.perales95@opendeusto.es>
2025-11-09 12:51:18 +0100

    remove unused plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,18 +20,5 @@ export default {
 			},
     },
   },
-  plugins: [
-    plugin(function({ addComponents, theme }) {
-      addComponents({
-        '.filled-background': {
-          background: `linear-gradient(to left, transparent 50%, ${theme('colors.primary')} 50%) right`,
-          backgroundSize: '201% 100%',
-          transition: '.5s ease-out',
-        },
-        '.filled-background:hover': {
-          backgroundPosition: 'left',
-        },
-      })
-    })
-  ],
+  plugins: [],
 }
